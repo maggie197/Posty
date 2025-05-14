6daf31473b2968831abf0e9f70df9292dbde1954
maggie197 <megijagabalina@gmail.com>
2025-05-14 16:35:00 +0100

    Added Photo model

---
+++ static/photo_models.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from your_app import db
+
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(120), nullable=False)
+    filepath = db.Column(db.String(200), nullable=False)
+    upload_time = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user = db.relationship('User', backref=db.backref('photos', lazy=True))
